61b1248839110c0a8b75e137f8feb7d694d6dbb4
Artem Golicyn <win.rus44@gmail.com>
2023-04-11 19:48:03 +0300

    New interface, added trend chart

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,14 @@
               </option>
             </select>
           </div>
+          <div class="main-menu">
+            <button v-if="showWordCloud" @click="showWordCloud = !showWordCloud" class="btn btn-light wordcloud-button" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse-cloud" aria-expanded="false" aria-controls="PersonsCollapse OrganizationsCollapse">Hide word-clouds</button>
+            <button v-else @click="showWordCloud = !showWordCloud" class="btn btn-outline-light wordcloud-button" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse-cloud" aria-expanded="false" aria-controls="PersonsCollapse OrganizationsCollapse">Show word-clouds</button>
+            <button v-if="showChartBar" @click="showChartBar = !showChartBar" class="btn btn-outline-light wordcloud-button" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse-chart" aria-expanded="false" aria-controls="ChartCollapse" ref="btn">Show chart</button>
+            <button v-else @click="showChartBar = !showChartBar" class="btn btn-light wordcloud-button" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse-chart" aria-expanded="false" aria-controls="ChartCollapse">Hide chart</button>
+            <button v-if="showTextInfo" @click="showTextInfo = !showTextInfo" class="btn btn-light wordcloud-button" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse-text" aria-expanded="false" aria-controls="TextCollapse">Hide text-info</button>
+            <button v-else @click="showTextInfo = !showTextInfo" class="btn btn-outline-light wordcloud-button" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse-text" aria-expanded="false" aria-controls="TextCollapse">Show text-info</button>
+          </div>
         </form>
       </div>
     </nav>
@@ -24,71 +32,138 @@
   <div class="main">
     <div>
       <div v-if="frameTitles" class="container text-center">
-        <label for="image-container"><strong>Wordcloud</strong></label>
-        <div class="image-container" id="image-container">
-          <picture>
-            <img class="wordcloud" id="wordcloud-locations"
+      <div v-if="showWordCloud" class="text-secondary"><h5>Today's word-clouds</h5></div>
+      <div class="row">
+        <div class="col">
+          <div aria-expanded="true" class="collapse multi-collapse-cloud show" id="LocationCollapse">
+            <div class="card card-body">
+              <span class="cloud-title">States, cities</span>
+              <div class="image-container" id="image-container">
+                <picture>
+                  <img class="wordcloud" id="wordcloud-locations"
                  v-bind:src="getPath('LOC')"
                  alt="LOCATIONS"
                  title="LOCATIONS WORDCLOUD">
-            <img class="wordcloud"
-                 v-bind:src="getPath('PER')"
-                 alt="PERSONS"
-                 title="PERSONS WORDCLOUD">
-            <img class="wordcloud"
-                 v-bind:src="getPath('ORG')"
-                 alt="ORGANIZATIONS"
-                 title="ORGANIZATIONS WORDCLOUD">
-          </picture>
+                </picture>
+              </div>
+            </div>
+          </div>
         </div>
-        <label for="text-container"><strong>Text</strong></label>
-        <div class="row justify-content-md-center" id="text-container">
-          <div class="col col-lg-2">
-            <label for="main-locations">Locations</label>
-            <div id="main-locations" class="list-group" v-for="(index, name) in entities.LOC" :key="index">
-              <a @click="getNews(name, 'LOC', $event)" href="#" class="list-group-item d-flex list-group-item-action">
-                <div class="d-flex w-100 justify-content-between">
-                  <small class="mb-1">{{ name }}</small>
-                  <span class="badge bg-primary rounded-pill">{{ getLenEntity(name, 'LOC') }}</span>
-                </div>
-              </a>
+        <div class="col">
+          <div class="collapse multi-collapse-cloud show" id="PersonsCollapse">
+            <div class="card card-body">
+              <span class="cloud-title">People</span>
+              <div class="image-container" id="image-container">
+                <picture>
+                  <img class="wordcloud"
+                    v-bind:src="getPath('PER')"
+                    alt="PERSONS"
+                    title="PERSONS WORDCLOUD">
+                </picture>
+              </div>
             </div>
           </div>
-          <div class="col col-lg-2">
-            <label for="main-persons">Persons</label>
-            <div id="main-persons" class="list-group" v-for="(index, name) in entities.PER" :key="index">
-              <a @click="getNews(name, 'PER', $event)" href="#" class="list-group-item d-flex list-group-item-action">
-                <div class="d-flex w-100 justify-content-between">
-                  <small class="mb-1">{{ name }}</small>
-                  <span class="badge bg-primary rounded-pill">{{ getLenEntity(name, 'PER') }}</span>
-                </div>
-              </a>
+        </div>
+        <div class="col">
+          <div class="collapse multi-collapse-cloud show" id="OrganizationsCollapse">
+            <div class="card card-body">
+              <span class="cloud-title">Organizations</span>
+              <div class="image-container" id="image-container">
+                <picture>
+                  <img class="wordcloud"
+                    v-bind:src="getPath('ORG')"
+                    alt="ORGANIZATIONS"
+                    title="ORGANIZATIONS WORDCLOUD">
+                </picture>
+              </div>
             </div>
           </div>
-          <div class="col col-lg-2">
-            <label for="main-organizations">Organizations</label>
-            <div id="main-organizations" class="list-group" v-for="(index, name) in entities.ORG" :key="index">
-              <a @click="getNews(name, 'ORG', $event)" href="#" class="list-group-item d-flex list-group-item-action">
-                <div class="d-flex w-100 justify-content-between">
-                  <small class="mb-1">{{ name }}</small>
-                  <span class="badge bg-primary rounded-pill">{{ getLenEntity(name, 'ORG') }}</span>
+        </div>
+      </div>
+      <div v-if="!showChartBar" class="text-secondary"><h5>Entity monthly trend</h5></div>
+      <div class="row">
+        <div class="col">
+          <div aria-expanded="true" class="collapse multi-collapse-chart" id="ChartCollapse">
+            <div class="card card-body">
+              <div v-if="showChart" class="chart-container">
+                <div class="chart-view">
+                  <Line :data="chartData" :options="chartOptions" ref="line"/>
+                  <form @submit.prevent class="form-inline">
+                      <div class="form-row row">
+                        <div class="form-group col-md-3">
+                          <input v-model="token" placeholder="Enter token, ex.: Москва" class="form-control input-size">
+                        </div>
+                        <div class="form-group col-md-3">
+                          <button v-if="tokenLength < 2" disabled @click="addDatasetChart(token)" class="btn btn-secondary mb-2 btn-chart">Add</button>
+                          <button v-else @click="addDatasetChart(token)" class="btn btn-secondary mb-2 btn-chart">Add</button>
+                          <button type="submit" @click="deleteChart" class="btn btn-secondary mb-2 btn-chart">Delete</button>
+                        </div>
+                      </div>
+                  </form>
                 </div>
-              </a>
+              </div>
             </div>
           </div>
-          <div class="col col-xl">
-            <label for="main-titles">News/Titles</label>
-            <div id="main-titles" class="list-group" v-for="title in titles.data" :key="title">
-              <a :href="title.href" class="list-group d-flex list-group-item-action" target="_blank">
-                <div class="d-flex justify-content-between">
-                  <small class="title-item">{{ title.title }}</small>
+        </div>
+      </div>
+      <div v-if="showTextInfo" class="text-secondary"><h5>Main entities</h5></div>
+      <div class="row">
+        <div class="col">
+          <div aria-expanded="true" class="collapse multi-collapse-text show" id="TextCollapse">
+            <div class="card card-body">
+              <div class="row justify-content-md-center" id="text-container">
+                <div class="col col-lg-2">
+                  <label class="cloud-title" for="main-locations">Locations</label>
+                  <div id="main-locations" class="list-group" v-for="(index, name) in entities.LOC" :key="index">
+                    <a @click="getNews(name, 'LOC', $event)"
+                       href="#"
+                       class="list-group-item d-flex list-group-item-action">
+                      <div class="d-flex w-100 justify-content-between">
+                        <small class="mb-1">{{ name }}</small>
+                        <span class="badge bg-secondary rounded-pill">{{ getLenEntity(name, 'LOC') }}</span>
+                      </div>
+                    </a>
+                  </div>
                 </div>
-              </a>
+                <div class="col col-lg-2">
+                  <label class="cloud-title" for="main-persons">Persons</label>
+                  <div id="main-persons" class="list-group" v-for="(index, name) in entities.PER" :key="index">
+                    <a @click="getNews(name, 'PER', $event)" href="#" class="list-group-item d-flex list-group-item-action">
+                      <div class="d-flex w-100 justify-content-between">
+                        <small class="mb-1">{{ name }}</small>
+                        <span class="badge bg-secondary rounded-pill">{{ getLenEntity(name, 'PER') }}</span>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div class="col col-lg-2">
+                  <label class="cloud-title" for="main-organizations">Organizations</label>
+                  <div id="main-organizations" class="list-group" v-for="(index, name) in entities.ORG" :key="index">
+                    <a @click="getNews(name, 'ORG', $event)" href="#" class="list-group-item d-flex list-group-item-action">
+                      <div class="d-flex w-100 justify-content-between">
+                        <small class="mb-1">{{ name }}</small>
+                        <span class="badge bg-secondary rounded-pill">{{ getLenEntity(name, 'ORG') }}</span>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div class="col col-xl">
+                  <label class="cloud-title" for="main-titles">News/Titles</label>
+                  <div id="main-titles" class="list-group" v-for="title in titles.data" :key="title">
+                    <a :href="title.href" class="list-group d-flex list-group-item-action" target="_blank">
+                      <div class="d-flex justify-content-between">
+                        <small class="title-item">{{ title.title }}</small>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   </div>
   <footer class="d-flex flex-wrap justify-content-between align-items-center mt-auto">
     <a href="https://github.com/CodNaoborot" target="_blank" class="contacts mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">GitHub</a>
@@ -97,15 +172,55 @@
 </template>
 
 <script>
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
 import axios from 'axios';
 
 export default {
   name: 'App',
   created() {
     this.getEntities()
+    document.cookie = "new=Cook"
+  },
+  computed: {
+     chartData() {return this.cData},
+     chartOptions() {return this.cOptions},
+     tokenLength() {return this.token.length},
+  },
+  components: {
+    Line
   },
   data() {
     return {
+      COLORS: [ '#4dc9f6', '#f67019', '#f53794', '#537bc4', '#acc236',
+                '#166a8f', '#00a950', '#58595b', '#8549ba' ],
+      token: '',
+      showWordCloud: true,
+      showTextInfo: true,
+      showChart: true,
+      showChartBar: true,
+      chart_axes: [],
       entities: {},
       titles: [],
       CurrentDate: new Date().toISOString().slice(0,10),
@@ -116,6 +231,19 @@ export default {
       ],
       selectedCountry: "Russia",
       frameTitles: false,
+      cOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        type: Object,
+        default: () => {}
+      },
+      cData: {
+        datasets: [
+          {
+            data: []
+          }
+        ]
+      }
     }
   },
   methods: {
@@ -132,6 +260,100 @@ export default {
       else {
         return require(`../../media/`+base_path)
       }
+    },
+    async getChartData(name) {
+      this.token = ''
+      let url
+      if (process.env.NODE_ENV === 'production') {
+        url = 'https://newsmakers.online/api/trend'
+      }
+      else {
+        url = 'http://0.0.0.0:8000/trend'
+      }
+      console.log(url)
+      return axios.post(url, {
+        country: this.selectedCountry,
+        date: this.CurrentDate,
+        token: name,
+        day_offset: 30,
+      })
+        .then((res) => {
+            this.chart_axes.push(res.data)
+            console.log(res)
+            })
+        .catch((e) => {
+          console.error(e)
+        })
+    },
+    async createChart(token) {
+      if (!(this.$refs.btn === null)) {
+          this.$refs.btn.click()
+      }
+      this.chart_axes = []
+      console.log("try to get data")
+      await this.getChartData(token)
+      console.log("New AXES", this.chart_axes[0])
+
+      let chart_labels = []
+      let chart_axis = []
+      for (let item of this.chart_axes[0]) {
+            chart_axis.push(item["weight"])
+            chart_labels.push(item["date"])
+          }
+      chart_axis.reverse()
+      chart_labels.reverse()
+      console.log(chart_labels)
+      console.log(chart_axis)
+
+      this.cData = {
+        ...this.cData,
+        labels: chart_labels,
+        datasets: [{
+            "data": chart_axis,
+            "label": token,
+            "borderColor": this.COLORS[0],
+            "backgroundColor": this.COLORS[0],
+        }]
+      }
+      console.log("NEW CHART", this.cData)
+      this.showChart = true
+      return this.cData
+
+    },
+    async addDatasetChart(token) {
+        this.showChartBar = false
+        if (this.cData.labels === undefined) {
+            return this.createChart(token)
+        }
+        await this.getChartData(token)
+        let chart_axis = []
+        let len = this.chart_axes.length - 1
+        for (let item of this.chart_axes[len]) {
+              chart_axis.push(item["weight"])
+            }
+        chart_axis.reverse()
+        console.log(chart_axis)
+
+        this.cData = {
+          ...this.cData,
+          datasets: this.cData.datasets.concat({
+              "data": chart_axis,
+              "label": token,
+              "borderColor": this.COLORS[len],
+              "backgroundColor": this.COLORS[len],
+          }),
+        }
+        console.log("NEW CHART", this.cData)
+        this.showChart = true
+        return this.cData
+    },
+    deleteChart() {
+        this.cData = {
+            ...this.cData,
+            datasets: this.cData.datasets.slice(0, this.cData.datasets.length - 1)
+        }
+        this.chart_axes.pop()
+
     },
     getEntities () {
       let url
@@ -172,11 +394,11 @@ export default {
       })
         .then((res) => {
           this.titles = res
-          console.log(res)
             })
         .catch((e) => {
           console.error(e)
         })
+      this.createChart(name)
     },
     getLenEntity(name, ent) {
       return (Array.from(this.entities[ent][name])).length
@@ -195,7 +417,6 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 10px 10px 4px;
   color: bisque;
@@ -243,7 +464,7 @@ nav a.router-link-exact-active {
   display: inline-block;
 }
 .wordcloud {
-  width: 30%;
+  width: 100%;
 }
 footer {
   position: relative;
@@ -266,6 +487,42 @@ footer {
 #text-container {
   margin-bottom: 20px;
 }
-
+.chart-view {
+    display: block;
+    box-sizing: border-box;
+    height: 250px;
+    width: 100%;
+}
+.wordcloud-button {
+    position: relative;
+    left: 30%;
+}
+.btn-secondary {
+    --bs-btn-bg: #303b44;
+}
+.btn-light {
+    margin-right: 10px;
+    padding: 3px 6px;
+}
+.btn-outline-light {
+    margin-right: 10px;
+    padding: 3px 6px;
+}
+.chart-container {
+    margin: 0 0 30px 0;
+    padding-bottom: 10px;
+}
+.col {
+    margin-bottom: 20px;
+}
+.cloud-title {
+    background-color: #6c757d;
+    color: bisque;
+    width: 100%;
+}
+.btn-chart {
+    margin-left: 10px;
+    width: 100px;
+}
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 // import axios from 'axios';
 import App from './App.vue'
 // import router from './router'
